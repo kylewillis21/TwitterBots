da58e6669cd0ce87142a920fe1e899b2fb1b3c05
kylewillis21 <44785217+kylewillis21@users.noreply.github.com>
2020-02-29 12:16:00 -0500

    Censored file for the access token

---
+++ accessToken.py
@@ -0,0 +1,7 @@
+# Access token and consumer key to Twitter API
+# Apply to be a Twitter Developer in order to get your own
+
+ACCESS_TOKEN = "Access Token here"
+ACCESS_SECRET = "Access Key here"
+CONSUMER_KEY = "Consumer Key"
+CONSUMER_SECRET = "Consumer secret"
